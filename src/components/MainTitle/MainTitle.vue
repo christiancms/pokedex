6b0f4676b5cb92448ec54bee60397e7d796f759c
christiancms <christian.souza@gmail.com>
2023-11-10 16:41:55 -0300

    Pages and layout

---
+++ src/components/MainTitle/MainTitle.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="title-inside text--white">
+        <h3>KevinGameTuber Mackedanz De Souza</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'MainTitle',
+    };
+</script>
+
+<style lang="scss" scoped>
+.title-inside {
+    display: flex;
+    justify-content: flex-end;
+    padding-bottom: 20px;
+    align-items: end;
+    flex-direction: column;
+}
+</style>
